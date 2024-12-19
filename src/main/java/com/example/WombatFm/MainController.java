903e124e53540c4a4f2533df16d58fd9a8e53a0f
ALEXANDER STANISLAUS BILIANTARA <alexanderbilliantara@gmail.com>
2024-12-19 10:13:58 +0700

    add controller

--- src/main/java/com/example/WombatFm/MainController.java
+++ src/main/java/com/example/WombatFm/MainController.java
@@ -12,4 +12,20 @@ public class MainController {
     public String index() {
         return "Main";
     }
+
+    @GetMapping("/setlist")
+    public String showSetlist() {
+        return "Setlist";
+    }
+
+    @GetMapping("/artist")
+    public String showArtistPage() {
+        return "Artist";
+    }
+
+    @GetMapping("/event")
+    public String showEventPage() {
+        return "Event";
+    }
+
 }
