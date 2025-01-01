e495f071e027bc7bd915ed7c7c57987042049edd
Syalom Elvin Pasau <syalomelvinpasau@gmail.com>
2025-01-01 23:47:25 +0700

    feat/add-setlist

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,24 +1,69 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const openModalButton = document.getElementById('openModal');
-  const modal = document.getElementById('popup-modal');
-  const closeModalButton = document.querySelector('.close-modal');
+  const buttonSearchShow = document.getElementById('search-show');
+  const buttonSearchArtist = document.getElementById('search-artist');
+  const modalShow = document.getElementById('popup-modal-show');
+  const modalArtist = document.getElementById('popup-modal-artist');
   const searchField = document.getElementById('search-field');
+  const closeShow = document.querySelector('.close-modal-show');
+  const closeArtist = document.querySelector('.close-modal-artist');
+  const optionArtist = document.querySelectorAll('.option-artist');
+  const optionShow = document.querySelectorAll('.option-show');
+  const fieldArtist = document.getElementById('artist');
+  const fieldShow = document.getElementById('show');
 
-  // Open the modal
-  openModalButton.addEventListener('click', () => {
-    modal.classList.add('visible');
-    searchField.focus(); // Focus on the input field
-  });
 
-  // Close the modal
-  closeModalButton.addEventListener('click', () => {
-    modal.classList.remove('visible');
-  });
 
-  // Optional: Close the modal when clicking outside of it
-  window.addEventListener('click', (event) => {
-    if (event.target === modal) {
-      modal.classList.remove('visible');
+  buttonSearchShow.addEventListener('click', () => {
+    if(modalArtist.style.display == 'flex'){
+      modalArtist.style.display = 'none';
     }
+
+    if(modalShow.style.display == 'flex'){
+      modalShow.style.display = 'none';
+    }
+    else{
+      modalShow.style.display = 'flex';
+      searchField.focus();
+    }
+  })
+
+  buttonSearchArtist.addEventListener('click', () => {
+    if(modalShow.style.display == 'flex'){
+      modalShow.style.display = 'none';
+    }
+
+    if(modalArtist.style.display == 'flex'){
+      modalArtist.style.display = 'none';
+    }
+    else{
+      modalArtist.style.display = 'flex';
+      searchField.focus();
+    }
+  })
+
+  closeShow.addEventListener('click', () => {
+    modalShow.style.display = 'none'; // Hide the modal
   });
+
+  closeArtist.addEventListener('click', () => {
+    modalArtist.style.display = 'none';
+  });
+
+  optionArtist.forEach(artist => {
+    artist.addEventListener('click', () => {
+      fieldArtist.value = artist.dataset.value;
+      console.log(artist.dataset.value);
+    });
+  });
+
+  optionShow.forEach(show => {
+    show.addEventListener('click', () => {
+      fieldShow.value = show.dataset.value;
+
+      console.log(show.dataset.value);
+    });
+  });
+
+
+
 });
