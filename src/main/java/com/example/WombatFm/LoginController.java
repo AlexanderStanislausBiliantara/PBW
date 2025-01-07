796115eea80decba8db776ad8cd73cabf9368397
Syalom Elvin Pasau <syalomelvinpasau@gmail.com>
2025-01-07 20:23:07 +0700

    fix/login bindingResult

--- src/main/java/com/example/WombatFm/LoginController.java
+++ src/main/java/com/example/WombatFm/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -27,18 +28,18 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password, HttpSession session, BindingResult bindingResult) {
+    public String login(@ModelAttribute("loginForm") LoginForm loginForm, BindingResult bindingResult ,HttpSession session) {
         if(bindingResult.hasErrors()) {
             return "Login";
         }
         
-        User user = userService.login(username, password);
+        User user = userService.login(loginForm.getUsername(), loginForm.getPassword());
         if(user == null) {
             bindingResult.reject("userNotFound", "Please check username or password");
             return "Login";
         }
 
-        session.setAttribute("username", username);
+        session.setAttribute("username", user.getUsername());
         session.setAttribute("role", user.getRole());
         return "redirect:";
     }
